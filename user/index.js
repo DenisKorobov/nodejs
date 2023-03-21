2c91c7a9b4daaae9ff70d241798ef2942d110c19
Коробов Денис Юрьевич <korobov@t-dev.pro>
2023-03-21 09:47:23 +0500

    update

---
+++ user/index.js
@@ -0,0 +1,20 @@
+var db = require('db');
+var log = require('logger')(module);
+
+function User(name) {
+    this.name = name;
+}
+
+User.prototype.hello = function (who) {
+    log(db.getPhrase('Hello') + ', ' + who.name);
+}
+
+log('user.js is required');
+
+// exports.User = User;
+
+// global.User = User;
+
+module.exports = User;
+
+// console.log(module);
